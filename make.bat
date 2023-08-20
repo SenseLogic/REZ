035b5efee2259ddc607d9df64095be68bcb89dea
senselogic <dev.senselogic@gmail.com>
2023-08-20 21:05:01 +0200

    % png

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-dmd -m64 rez.d color.d png.d
+dmd -m64 rez.d core.d color.d png.d
 del *.obj
 pause
--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
-dmd -m64 rez.d color.d png.d
+dmd -m64 rez.d core.d color.d png.d
 rm *.o
